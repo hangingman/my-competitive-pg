41d24054676e791d76b8299edaaabf6186475886
hiroyuki nagata <newserver002@gmail.com>
2017-08-30 22:39:44 +0900

    ABC 062 D, 解答を見て部分点を取れる形にした

--- atcoder/abc/062/D.rb
+++ atcoder/abc/062/D.rb
@@ -1,27 +1,44 @@
+# coding: utf-8
 lines = <<'EOS'
 2
 3 1 4 1 5 9
 EOS
 
+#
+# a の前半 k 要素は黒色または赤色のみで
+# a の後半 3N − k 要素は黒色または青色のみである．
+#
+# N ≤ k ≤ 2N
+#
+# [3, 1, 4, 1, 5, 9]
+#
+# [3,    4, 1, 5,  ]
+#
+# <---k--><--3N-k-->
+# 前半k要素の総和が大きくなればスコアは最大になる
+
+
 #lines = $stdin.read
 array = lines.split("\n")
 N = array[0].to_i
-vals = array[1].split(" ").map(&:to_i)
-keys = [*0...vals.size].map(&:to_i)
+a = array[1].split(" ").map(&:to_i)
 
-hash = Hash[keys.zip vals]
-scores = []
+score = []
 
-keys.combination(N).each do |elems|
-  hash_cp = hash.dup
-  elems.each do |k,v|
-    hash_cp.delete(k)
-  end
-  #puts hash_cp.values.to_s
-  a_dash = hash_cp.values
-  score  = a_dash[0, N].inject(:+) - a_dash[-N, N].inject(:+)
-  #puts score
-  scores << score
+for k in N..2*N
+  fst = a[0,k].sort.reverse
+  #puts "#{fst}"
+
+  sec = a[k,3*N-k].sort
+  #puts "#{sec}"
+
+  s = fst[0, N].inject(&:+)
+  l = sec[0, N].inject(&:+)
+
+  #puts "#{s} - #{l} = #{s-l}"
+  #puts ""
+
+  score << s - l
 end
 
-puts scores.max
+puts score.max
