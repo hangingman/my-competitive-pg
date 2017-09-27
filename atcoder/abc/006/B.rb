dbebec16e27296913e56ccd5bffde029794e2065
hiroyuki.nagata <hiroyuki.nagata@veeva.com>
2017-09-27 16:19:59 +0900

    Commit abc 006 - B ... TLE

---
+++ atcoder/abc/006/B.rb
@@ -0,0 +1,33 @@
+#lines = <<'EOS'
+#2
+#EOS
+
+lines = $stdin.read
+array = lines.split("\n")
+n = array[0].to_i
+
+#start_time = Time.now
+
+def tribonacci(n)
+
+  if n == 1 or n == 2
+    0
+  elsif n == 3
+    1
+  else
+    t0 = 0
+    t1 = 0
+    t2 = 1
+    tn = 0
+    4.upto(n) do
+      tn = t0 + t1 + t2
+      t0,t1,t2 = t1,t2,tn
+    end
+    tn % 10007
+  end
+
+end
+
+puts tribonacci(n)
+
+#p "Elasped #{Time.now - start_time}s"
