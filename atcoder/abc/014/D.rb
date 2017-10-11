f9cc3d68497ab1d42bae2a0756c6638c8cf8d9ef
hiroyuki.nagata <hiroyuki.nagata@veeva.com>
2017-10-11 16:32:21 +0900

    Commit AtCoder abc 014 - D ... TLE but 30point

--- atcoder/abc/014/D.rb
+++ atcoder/abc/014/D.rb
@@ -10,6 +10,22 @@ lines = <<'EOS'
 2 4
 EOS
 
+#lines = <<'EOS'
+7
+3 1
+2 1
+2 4
+2 5
+3 6
+3 7
+5
+4 5
+1 6
+5 6
+4 7
+5 3
+EOS
+
 #lines = $stdin.read
 array = lines.split("\n")
 
@@ -19,56 +35,68 @@ class Array
   alias_method :dequeue, :shift
 end
 
-Node = Struct.new(:u, :k, :v, :visited)
+#
+# u : Index of nodes starting from 1...N
+# k : degree of this node
+# v : array of vertices connected with this node
+# c : :white(=undiscovered), :gray(=frontier), :black(finished searching)
+# d : distance when the vertex is first discovered
+# pi: predecessor vertex
+Node = Struct.new(:u, :k, :v, :c, :d, :pi)
 
 class Graph
 
-  attr :nodes, :dist
+  attr :nodes, true
 
   def initialize(n)
     @nodes = Array.new(n){ Node.new }
-    @dist = Array.new(n)
   end
 
   def add_graph_node(u, v)
     @nodes[u-1].u = u
     (@nodes[u-1].v ||= []).push(v)
     @nodes[u-1].k = @nodes[u-1].v.length
-    @nodes[u-1].visited = false
+    @nodes[u-1].c = :white
+    @nodes[u-1].d = -1
+    @nodes[u-1].pi = nil
   end
 
   def reset()
     @nodes.each do |node|
-      node.visited = false if not node.visited.nil?
+      node.c  = :white
+      node.d  = -1
+      node.pi = nil
     end
   end
 
-  def bfs(start = 0, queue)
+  def dist()
+    puts "d : #{@nodes.map{|node| node.d}.to_a.to_s}"
+    puts "pi: #{@nodes.map{|node| node.pi}.to_a.to_s}"
+  end
 
-    @nodes[start].visited = true
-    #puts "visit = #{@nodes[start].u}, queue = #{queue.to_s}"
-    @dist[start] = 0
-    #puts "dist: @dist[#{start}] = 0"
-    queue.enqueue(@nodes[start])
-    #puts "enq = #{@nodes[start].u}, queue = #{queue.to_s}"
+  def bfs(start, queue)
+
+    start_idx = (start - 1).to_i
+    @nodes[start_idx].c  = :gray
+    @nodes[start_idx].d  = 0
+    @nodes[start_idx].pi = nil
+    queue.enqueue(@nodes[start_idx])
 
     while not queue.empty?
 
-      node = queue.dequeue
-      break if node.v.nil?
+      u = queue.dequeue
+      break if u.v.nil?
 
-      #puts "deq = #{node.u}, queue = #{queue.to_s}"
-      for i in node.v
-        unless @nodes[i-1].visited
-          @nodes[i-1].visited  = true
-          queue.enqueue(@nodes[i-1])
-          #puts "enq = #{@nodes[i-1].u}, queue = #{queue.to_s}"
-          if not @nodes[i-1].u.nil?
-            @dist[@nodes[i-1].u-1] = @dist[node.u-1] + 1
-            #puts "dist: @dist[#{@nodes[i-1].u-1}] = #{@dist[node.u - 1] + 1}"
-          end
+      u.v.each do |label|
+        v = @nodes[label-1]
+        if v.c == :white
+          v.c  = :gray
+          v.d  = u.d.to_i + 1
+          v.pi = u
+          queue.enqueue(v)
         end
       end
+      u.c = :black
     end
   end
 end
@@ -83,29 +111,21 @@ graph = Graph.new(N)
 
 for i in 1...N
   seps = array[i].split(" ")
-  u = i
-  v = seps[1].to_i
+  u,v  = seps.map(&:to_i) # .map{|n| n-1}
+  #puts "add_graph_node(#{u} <--> #{v})"
   graph.add_graph_node(u, v)
   graph.add_graph_node(v, u)
 end
 
-#puts graph.nodes.to_s
+# graph.nodes.each do |node|
+#     puts node.to_s
+# end
 
 for j in N+1...N+Q+1
   src,dst = array[j].split(" ").map(&:to_i)
-  puts "#{src} to #{dst}"
+  # puts "#{src} to #{dst}"
   graph.reset()
-  graph.bfs(src - 1, [])
-  puts graph.dist.to_s
-  puts graph.dist[dst-1].to_i + 1
+  graph.bfs(src, [])
+  #puts graph.dist
+  puts graph.nodes[dst.to_i-1].d.to_i + 1
 end
-
-#graph.bfs(start = 0, [])
-#
-#1.upto(N).each do |n|
-#  if graph.dist[n-1].nil?
-#    puts "#{n} -1"
-#  else
-#    puts "#{n} #{graph.dist[n-1]}"
-#  end
-#end
