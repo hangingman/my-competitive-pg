b4aac9a484f61843329d08453a4f32c66efd394b
hiroyuki.nagata <hiroyuki.nagata@veeva.com>
2017-10-13 09:09:11 +0900

    Fix bug on 782b3d4e2663257b4c05990a3e6dad7af0af0cdf

--- atcoder/abc/013/D.rb
+++ atcoder/abc/013/D.rb
@@ -1,5 +1,5 @@
 lines = <<'EOS'
-5 7 1
+5 7 2
 1 4 3 4 2 3 1
 EOS
 
@@ -9,10 +9,22 @@ array = lines.split("\n")
 N,M,D = array[0].split(" ").map(&:to_i)
 A     = array[1].split(" ").map(&:to_i)
 
+st    = 1.upto(N).to_a
 to    = 1.upto(N).to_a
 
 for a in A.reverse
   to[a], to[a-1] = to[a-1],to[a]
 end
 
-to.each{ |t| puts t }
+1.upto(D) do |i|
+  #puts "concat! D =#{i}"
+  neo_to = Array.new(N)
+
+  to.each_with_index do |elem,idx|
+    neo_to[idx] = st[elem-1]
+  end
+  st = neo_to
+  #puts st.to_s
+end
+
+st.each{ |t| puts t }
