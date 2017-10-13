a1239db5f89445b120dcd2d23c0c427a0b93e857
Hiroyuki Nagata <hiroyuki.nagata@veeva.com>
2017-10-13 15:06:54 +0900

    Commit AtCoder abc 013 - D ... 30pt

--- atcoder/abc/013/D.rb
+++ atcoder/abc/013/D.rb
@@ -6,6 +6,11 @@ EOS
 #lines = $stdin.read
 array = lines.split("\n")
 
+def fact(n, a=1)
+  return a if n == 0
+  fact(n-1, n*a)
+end
+
 N,M,D = array[0].split(" ").map(&:to_i)
 A     = array[1].split(" ").map(&:to_i)
 
@@ -16,15 +21,19 @@ for a in A.reverse
   to[a], to[a-1] = to[a-1],to[a]
 end
 
-1.upto(D) do |i|
-  #puts "concat! D =#{i}"
-  neo_to = Array.new(N)
+if D != 1
+  d = D % fact(N)
+  1.upto(d) do |i|
+    #puts "concat! D =#{i}"
+    neo_to = Array.new(N)
 
-  to.each_with_index do |elem,idx|
-    neo_to[idx] = st[elem-1]
+    to.each_with_index do |elem,idx|
+      neo_to[idx] = st[elem-1]
+    end
+    st = neo_to
+    #puts st.to_s
   end
-  st = neo_to
-  #puts st.to_s
+  st.each{ |t| puts t }
+else
+  to.each{ |t| puts t }
 end
-
-st.each{ |t| puts t }
