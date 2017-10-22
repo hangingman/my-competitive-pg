5e026755634f98c67b70a708085e89ae8d719678
hiroyuki nagata <newserver002@gmail.com>
2017-10-22 10:49:17 +0900

    Start implementing prim according to faculty.ycp.edu http://faculty.ycp.edu/~dbabcock/PastCourses/cs360/lectures/lecture20.html

--- aizu/ALDS/ALDS1_12_A.rb
+++ aizu/ALDS/ALDS1_12_A.rb
@@ -7,29 +7,37 @@ lines = <<'EOS'
  -1 -1 1 3 -1
 EOS
 
+Node = Struct.new(:u, :key, :pi)
+
+class Graph
+  attr :nodes, :mat
+  def initialize(n, matrix)
+    @nodes = Array.new(n){ Node.new }
+    @mat   = matrix.dup
+  end
+
+  def prim(weight, root)
+    for node in @nodes
+      node.key = Float::INFINITY
+      node.pi  = nil
+    end
+    root.key = 0
+  end
+end
+
 #lines = $stdin.read
 array = lines.split("\n")
 
 N   = array[0].to_i
-d,p = Array.new(N){0},Array.new(N){-1}
-c   = Array.new(N){:white}
-m   = Array.new(N).map{Array.new(N, 0)}
+mat = Array.new(N).map{Array.new(N, 0)}
 
 for i in 1...N
   row  = i - 1
   cols = array[i].split(" ").map(&:to_i)
   cols.each_with_index do |col, idx|
-    m[row][col] = col
+    mat[row][col] = col
   end
 end
 
-puts m.to_s
-
-while true
-  mincost = nil
-  for i in 0...N-1
-    if
-
-    end
-  end
-end
+graph = Graph.new(N, mat)
+graph.prim(1, graph.nodes.first)
