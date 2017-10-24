9563b56fe5e0b29b115107abb6d659e479c30fe2
hiroyuki.nagata <hiroyuki.nagata@veeva.com>
2017-10-24 15:03:22 +0900

    Update prim method

--- aizu/ALDS/ALDS1_12_A.rb
+++ aizu/ALDS/ALDS1_12_A.rb
@@ -7,28 +7,67 @@ lines = <<'EOS'
  -1 -1 1 3 -1
 EOS
 
+INF  = 999999999
 Node = Struct.new(:u, :d, :c, :p)
 
 class Graph
   attr :nodes, :mat
   def initialize(n, matrix)
     @nodes = Array.new(n){ Node.new }
+    @nodes.each_with_index{|node,idx| node.u = idx+1}
     @mat   = matrix.dup
   end
 
+  def edge_exists?(row, col)
+    printf "  edge_exists?(#{row}, #{col}) => "
+    row -= 1
+    col -= 1
+    result = if @mat[row][col] != -1 # or @mat[col][row] != -1
+               true
+             else
+               false
+             end
+    puts "#{result} => cost: #{@mat[row][col]}"
+    result
+  end
+
   def prim(start = 1)
-    for node in @nodes
-      node.d = Float::INFINITY
+    @nodes = @nodes.each do |node|
+      node.d = INF
       node.p = nil
       node.c = :white
     end
     start = (start-1).to_i
     @nodes[start].d = 0
     @nodes[start].p = -1
+    next_u = nil
 
     while true
-      mincost = Float::INFINITY
-      break
+      mincost = INF
+
+      @nodes.each do |node|
+        if node.c != :black and node.d < mincost
+          mincost = node.d
+          next_u  = node.u
+        end
+      end
+      puts "mincost = #{mincost}, next_u = #{next_u}"
+
+      break if mincost == INF
+
+      @nodes[(next_u-1).to_i].c = :black
+
+      @nodes = @nodes.map.with_index(1) do |node, v|
+        if node.c != :black and edge_exists?(next_u, v) and mat[next_u-1][v-1] < node.d
+          node.d = mat[next_u-1][v-1]
+          node.p = next_u
+          node.c = :gray
+          puts "  update: node(#{node.u}), d = #{node.d}, parent = #{next_u}"
+          node
+        else
+          node
+        end
+      end
     end
   end
 end
@@ -43,9 +82,11 @@ for i in 1...N
   row  = i - 1
   cols = array[i].split(" ").map(&:to_i)
   cols.each_with_index do |col, idx|
-    mat[row][col] = col
+    mat[row][idx] = col
   end
 end
 
 graph = Graph.new(N, mat)
 graph.prim(1)
+
+puts graph.nodes.map{|n| n.to_s}.to_a
