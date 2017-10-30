13fb8ca15d12f2c19314808c902fc6ca74008dd5
hiroyuki.nagata <hiroyuki.nagata@veeva.com>
2017-10-30 13:18:59 +0900

    Fix undigraph to digrap

--- atcoder/abc/012/D.rb
+++ atcoder/abc/012/D.rb
@@ -1,6 +1,6 @@
 INF  = 999999999
 Node = Struct.new(:u, :d, :c, :p)
- 
+
 class Graph
   attr :nodes, :mat, :u_start
   def initialize(n, matrix, u_start_with = 1)
@@ -9,7 +9,7 @@ class Graph
     @u_start = u_start_with
     @mat   = matrix.dup
   end
- 
+
   def edge_exists?(row, col)
     #printf "  edge_exists?(#{row}, #{col}) => "
     row -= @u_start
@@ -22,7 +22,7 @@ class Graph
     #puts "#{result} => cost: #{@mat[row][col]}"
     result
   end
- 
+
   def gen_uniq_edges()
     edges = []
     @nodes.each_with_index do |node,i|
@@ -35,7 +35,7 @@ class Graph
     end
     edges.uniq
   end
- 
+
   def dot_before_dijkstra()
     puts "digraph beforeDijkstra {"
     gen_uniq_edges.each do |e|
@@ -43,11 +43,11 @@ class Graph
     end
     puts "}"
   end
- 
+
   def dot_after_dijkstra(start = 1)
     # start dijkstra
     dijkstra(start)
- 
+
     # then, generate dot
     puts "digraph afterDijkstra {"
     gen_uniq_edges.each do |e|
@@ -60,19 +60,19 @@ class Graph
     end
     puts "}"
   end
- 
+
   def dijkstra(start = 1)
     @nodes = @nodes.each do |node|
       node.d = INF
       node.p = nil
       node.c = :white
     end
- 
+
     start = (start-@u_start).to_i
     @nodes[start].d = 0
     @nodes[start].p = -1
     next_u = nil
- 
+
     while true
       mincost = INF
       @nodes.each do |node|
@@ -83,11 +83,11 @@ class Graph
         end
       end
       #puts "mincost = #{mincost}, next_u = #{next_u}"
- 
+
       break if mincost == INF
- 
+
       @nodes[next_u].c = :black
- 
+
       @nodes = @nodes.map.with_index do |node, v|
         if node.c != :black and edge_exists?(next_u, v) and @nodes[next_u].d + mat[next_u][v] < node.d
           node.d = @nodes[next_u].d + mat[next_u][v]
@@ -101,34 +101,40 @@ class Graph
     end
   end
 end
- 
+
 lines = <<'EOS'
-4 6
-1 2 1
-2 3 1
-3 4 1
-4 1 1
-1 3 1
-4 2 1
+5 5
+1 2 12
+2 3 14
+3 4 7
+4 5 9
+5 1 18
 EOS
 
 #lines = $stdin.read
 array = lines.split("\n")
- 
+
 N,M = array[0].split(" ").map(&:to_i)
 mat = Array.new(N).map{Array.new(N, -1)}
- 
-for i in 1...N
+
+for i in 1..M
   u,v,w = array[i].split(" ").map(&:to_i)
   mat[u-1][v-1] = w
-  mat[v-1][u-1] = w
+  #mat[v-1][u-1] = w
 end
- 
+
 # 0-indexed
 graph = Graph.new(N, mat, 0)
- 
+
 # start-with 0
-graph.dot_before_dijkstra()
+# graph.dot_before_dijkstra()
+# graph.nodes.each do |n|
+#   graph.dijkstra(n.u)
+#   puts "from #{n.u}"
+#   graph.nodes.each{|node| puts "  #{node.u} #{node.d}"}
+# end
+
 graph.dijkstra(0)
- 
-graph.nodes.each{|n| puts "#{n.u} #{n.d}"}
+max_d = graph.nodes.max_by {|n| n.d }.d
+max_t = graph.nodes.min_by {|n| (n.d - max_d / 2).abs }.d
+puts max_t
