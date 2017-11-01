c6ed7b44ad2ae3dd03dbe61b9542a626b43282a2
hiroyuki.nagata <hiroyuki.nagata@veeva.com>
2017-11-01 09:52:35 +0900

    Update template file generator

--- atcoder/begin.sh
+++ atcoder/begin.sh
@@ -11,11 +11,42 @@ fi
 
 mkdir -p "${contest}/${number}/"
 
-cat << EOT > ${contest}/${number}/template.rb
+# create ruby template files
+cat << EOT > ${contest}/${number}/A.rb
 lines = <<'EOS'
 2 4
 EOS
 
 #lines = \$stdin.read
 array = lines.split("\n")
+EOT
+
+cp ${contest}/${number}/A.rb ${contest}/${number}/B.rb
+cp ${contest}/${number}/A.rb ${contest}/${number}/C.rb
+
+# create dlang template files
+cat << EOT > ${contest}/${number}/D.d
+import std.stdio;
+import std.conv;
+import std.string;
+import std.format;
+import std.algorithm;
+
+void main()
+{
+
+  //string lines;
+  //string buf;
+  //
+  //while (!stdin.eof) {
+  //  buf = stdin.readln();
+  //  lines ~= buf;
+  //}
+
+  string lines = q"[
+  ]";
+
+  string[] array = splitLines(lines);
+  writeln(to!string(array));
+}
 EOT
