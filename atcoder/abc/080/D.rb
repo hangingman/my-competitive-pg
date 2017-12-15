be306f905d73c2965c466649b6643e512f6b7c7e
hiroyuki.nagata <hiroyuki.nagata@veeva.com>
2017-12-15 15:09:22 +0900

    Commit AtCoder ABC 080 D WA(20/22)

--- atcoder/abc/080/D.rb
+++ atcoder/abc/080/D.rb
@@ -12,26 +12,19 @@ TIME = 10**5
 
 N,C = array[0].split(" ").map(&:to_i)
 
-mat = Array.new(C).map{Array.new(TIME, 0)}
+vec = Array.new(TIME){0}
 
-array[1..array.length].each do |rec|
-  s,e,c = rec.split(" ").map(&:to_i)
-  mat[c-1][s] += 1
-  mat[c-1][e+1] -= 1
+array[1..N+1].each do |rec|
+  s,e,c  = rec.split(" ").map(&:to_i)
+  vec[s] = vec[s] + 1
+  vec[e+1] = vec[e+1] - 1 rescue vec[e+1]
 end
 
-#p mat
+ans = Array.new(vec.length){0}
+ans[0] = vec[0]
 
-mat.map! do |rec|
-  rec.map.reduce([]) do |acc, e|
-    acc << (acc == [] ? e : acc.last + e)
-  end
+for i in 1...ans.length
+  ans[i] = ans[i-1] + vec[i-1] rescue ans[i-1]
 end
 
-#mat.each{|rec| puts rec.to_s}
-
-add_mat = mat.transpose.map{|a| a.inject(:+) }
-
-#p add_mat
-
-puts add_mat.max
+puts ans.max
