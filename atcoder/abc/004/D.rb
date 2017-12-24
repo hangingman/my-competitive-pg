d40c374ccae408d8df821a8c1674887a9079c578
hiroyuki nagata <newserver002@gmail.com>
2017-12-25 02:34:26 +0900

    Add debug code

--- atcoder/abc/004/D.rb
+++ atcoder/abc/004/D.rb
@@ -1,5 +1,5 @@
 lines = <<'EOS'
-2 3 4
+267 294 165
 EOS
 
 #lines = $stdin.read
@@ -10,4 +10,16 @@ def marble(n)
 end
 
 R,G,B = array[0].split(" ").map(&:to_i)
-puts marble(R) + marble(G) + marble(B)
+# puts marble(R) + marble(G) + marble(B)
+
+cusum = Array.new(800){0}
+
+# index[-100] = 300
+# index[   0] = 400
+# index[+100] = 500
+
+puts "R/2 = #{R/2}, [l, r] = [#{300 - (R/2)}, #{300 + (R/2)}]"
+puts "G/2 = #{G/2}, [l, r] = [#{400 - (G/2)}, #{400 + (G/2)}]"
+puts "B/2 = #{B/2}, [l, r] = [#{500 - (B/2)}, #{500 + (B/2)}]"
+
+# p cusum
