eb597ea28bde3058024adfeee28880092f1a4aa3
hiroyuki nagata <newserver002@gmail.com>
2018-01-03 02:12:43 +0900

    Commit erastosthenes code

---
+++ algorithm/erastosthenes.rb
@@ -0,0 +1,15 @@
+def erastosthenes(n)
+  array = (2..n).to_a
+  threshold = Math.sqrt(n)
+  count = 2
+  while threshold > count
+    array.reject!{|n| n != count and n % count == 0}
+    count += 1
+  end
+  array
+end
+
+PN = erastosthenes(1000000)
+
+puts PN.length
+puts PN[10000]
