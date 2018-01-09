5a2161cb874ec9db63d1c82c492f9f60a40ac837
hiroyuki nagata <newserver002@gmail.com>
2018-01-10 00:12:11 +0900

    Add sample code for ABC 005 - D

--- atcoder/abc/005/D.rb
+++ atcoder/abc/005/D.rb
@@ -16,3 +16,30 @@ N = array[0].to_i
 D = array[1..N].map{|s| s.split(" ").map(&:to_i)}
 Q = array[N+1].to_i
 P = array[N+2..Q+N+1].map(&:to_i)
+
+I = J = W = H = N
+
+dp = Array.new(I).map{
+  Array.new(J).map{
+    Array.new(W).map{
+      Array.new(H, 0)
+    }
+  }
+}
+
+for i in 0..N-1
+  for j in 0..N-1
+    dp[i][j][0][0] = D[i][j]
+  end
+end
+
+for i in 0..N-1
+  for j in 0..N-1
+    for h in 1..N-1
+      # p dp[i][j][0][h-1]
+      # p dp[i+h][j][0][0]
+      dp[i][j][0][h] = dp[i][j][0][h-1] + dp[i+h][j][0][0] rescue 0
+      puts "dp[#{i}][#{j}][0][#{h}] = #{dp[i][j][0][h]}"
+    end
+  end
+end
