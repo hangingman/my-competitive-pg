31c739c598c0b81447456ea3dd8f8435769237fc
hiroyuki nagata <newserver002@gmail.com>
2018-01-14 00:42:37 +0900

    Update AtCoder ABC 001 A,B

--- atcoder/abc/001/A.rb
+++ atcoder/abc/001/A.rb
@@ -4,3 +4,8 @@ EOS
 
 #lines = $stdin.read
 array = lines.split("\n")
+
+H1 = array[0].to_i
+H2 = array[1].to_i
+
+puts H1 - H2
--- atcoder/abc/001/B.rb
+++ atcoder/abc/001/B.rb
@@ -1,6 +1,19 @@
 lines = <<'EOS'
-2 4
+40000
 EOS
 
 #lines = $stdin.read
 array = lines.split("\n")
+M = array[0].to_i
+
+if M < 100
+  puts "00"
+elsif 100 <= M and M <= 5000
+  printf "%02d\n", M/100
+elsif 6000 <= M and M <= 30000
+  printf "%d\n", M/1000 + 50
+elsif 35000 <= M and M <= 70000
+  printf "%d\n", (M/1000 - 30)/5 + 80
+else
+  puts "89"
+end
