7e332e9c817447590436b686d4306111ef3d0c82
hangedman <idiotpanzer@gmail.com>
2018-06-08 08:14:01 +0900

    Update D.rb
    AC, finally

--- atcoder/abc/066/D.rb
+++ atcoder/abc/066/D.rb
@@ -1,6 +1,6 @@
 # coding: utf-8
 MOD = 1_000_000_007
-
+ 
 class Combination
   def initialize
     @fact = (1..10**5+1).to_a
@@ -8,21 +8,21 @@ class Combination
       @fact[idx] = @fact[idx] * @fact[idx-1] % MOD if idx > 0
     end
   end
-
+ 
   def choose(n, r)
     return 0 if not (0 <= r and r <= n)
     return 1 if r == 0 or r == n
     fact(n) * inverse( fact(r) * fact(n-r) )
   end
-
+ 
   def fact(n)
     @fact[n-1]
   end
-
+ 
   def inverse(x)
     pow(x, MOD-2)
   end
-
+ 
   def pow(x, n)
     ans = 1
     while n > 0
@@ -34,23 +34,16 @@ class Combination
   end
 end
 
-lines = <<'EOS'
-32
-29 19 7 10 26 32 27 4 11 20 2 8 16 23 5 14 6 12 17 22 18 30 28 24 15 1 25 3 13 21 19 31 9
-EOS
-#lines = $stdin.read
+lines = $stdin.read
 array = lines.split("\n")
-
-N  = array[0].to_i
-A  = array[1].split(" ").map(&:to_i)
-
-DUPL = A.group_by{|i| i}.reject{|k,v| v.one?}.keys.first
-
-LEFT, RIGHT = A.index(DUPL), A.rindex(DUPL)
-DUPL_N = LEFT + (A.length - RIGHT - 1)
-
+ 
+$n = array[0].to_i
+$a = array[1].split(" ").map(&:to_i)
+ 
 @comb = Combination.new
 
-A.each_with_index do |elem, idx|
-  puts (@comb.choose(A.length, idx+1) % MOD - @comb.choose(DUPL_N, idx) % MOD)
+dup=$a.sort.each_cons(2).select{|a,b|a==b}.flatten[0]
+l,r=$a.index(dup),$a.rindex(dup)
+(1..($n+1)).each do |k|
+  puts (@comb.choose($n+1,k)-@comb.choose($n+1-(r-l+1),k-1)) % MOD
 end
