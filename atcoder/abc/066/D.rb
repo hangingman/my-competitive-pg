44a5978960d2c3987ce9a6243bb1a478c27b36c9
hiroyuki nagata <newserver002@gmail.com>
2018-06-08 00:30:54 +0900

    AtCoder ABC 066 - D , WA(4/10)

--- atcoder/abc/066/D.rb
+++ atcoder/abc/066/D.rb
@@ -1,12 +1,44 @@
 # coding: utf-8
-# lines = <<'EOS'
-# 32
-# 29 19 7 10 26 32 27 4 11 20 2 8 16 23 5 14 6 12 17 22 18 30 28 24 15 1 25 3 13 21 19 31 9
-# EOS
-
 MOD = 1_000_000_007
 
-lines = $stdin.read
+class Combination
+  def initialize
+    @fact = (1..10**5+1).to_a
+    @fact.each_with_index do |e, idx|
+      @fact[idx] = @fact[idx] * @fact[idx-1] % MOD if idx > 0
+    end
+  end
+
+  def choose(n, r)
+    return 0 if not (0 <= r and r <= n)
+    return 1 if r == 0 or r == n
+    fact(n) * inverse( fact(r) * fact(n-r) )
+  end
+
+  def fact(n)
+    @fact[n-1]
+  end
+
+  def inverse(x)
+    pow(x, MOD-2)
+  end
+
+  def pow(x, n)
+    ans = 1
+    while n > 0
+      ans = ans * x % MOD if n.odd?
+      x = x * x % MOD
+      n >>= 1
+    end
+    ans
+  end
+end
+
+lines = <<'EOS'
+32
+29 19 7 10 26 32 27 4 11 20 2 8 16 23 5 14 6 12 17 22 18 30 28 24 15 1 25 3 13 21 19 31 9
+EOS
+#lines = $stdin.read
 array = lines.split("\n")
 
 N  = array[0].to_i
@@ -14,16 +46,11 @@ A  = array[1].split(" ").map(&:to_i)
 
 DUPL = A.group_by{|i| i}.reject{|k,v| v.one?}.keys.first
 
-LEFT, RIGHT = A.map.with_index do |item,idx|
-  [idx, item]
-end.to_h.select do |k,v|
-  v == DUPL
-end.keys
-
+LEFT, RIGHT = A.index(DUPL), A.rindex(DUPL)
 DUPL_N = LEFT + (A.length - RIGHT - 1)
-DUPL_N_ARR = (1..DUPL_N).to_a
+
+@comb = Combination.new
 
 A.each_with_index do |elem, idx|
-  r = idx + 1
-  p (A.combination(r).size - DUPL_N_ARR.combination(r-1).size) % MOD
+  puts (@comb.choose(A.length, idx+1) % MOD - @comb.choose(DUPL_N, idx) % MOD)
 end
