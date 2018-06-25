a5eddc532ad461a871c3c11d6edce8be87bd01e2
hiroyuki nagata <newserver002@gmail.com>
2018-06-25 23:41:19 +0900

    Commit AtCoder ABC 038 - D WA

--- atcoder/abc/038/D.rb
+++ atcoder/abc/038/D.rb
@@ -1,10 +1,67 @@
+class FenwickTree
+  attr_accessor :n, :n2, :array, :left, :right
+
+  def initialize(n, array)
+    @n = n
+    n2 = 1
+    while n2 < n
+      n2 <<= 1
+    end
+    @n2 = n2
+    @left = Array.new(@n+1, 0)
+    @right = Array.new(@n+1, 0)
+
+    @array = array.dup
+    for i in 0...n
+      add(i+1,@array[i])
+    end
+  end
+
+  def G(x)
+    x - (x & (x-1))
+  end
+
+  def add(idx, x)
+    @array[idx-1]=x
+    i=idx
+    while i<=@n2
+      break if i>@n
+      @left[i] = [@left[i],x].max
+      i=i+G(i)
+    end
+    i=idx
+    while i>0
+      @right[i] = [@right[i],x].max
+      i=i-G(i)
+    end
+  end
+
+  # 1-indexed
+  def max(l, r)
+    ans=0
+    i=l
+    while i+G(i) <= r
+      ans=[ans,right[i]].max
+      i=i+G(i)
+    end
+
+    ans = i if @array[i-1] > ans
+    ans = [ans,@array[i-1]].max
+    i=r
+
+    while i-G(i) >= l
+      ans=[ans,left[i]].max
+      i=i-G(i)
+    end
+    ans
+  end
+end
+
 lines = <<'EOS'
-5
-8 8
-5 3
+3
+3 3
+1 1
 2 2
-4 2
-2 1
 EOS
 
 #lines = $stdin.read
@@ -13,33 +70,18 @@ N = array[0].to_i
 boxes = array[1..N].map do |str|
   arr = str.split(" ").map(&:to_i)
   Hash[*arr]
-end.sort_by {|k, v| k.first }
+end.sort_by do |e|
+  key,val = e.first
+  [key,-val]
+end.map{|m| m.values}.flatten
 
-p boxes
-dp = Array.new(N).map{Array.new(N, 0)}
-puts ""
+dp = Array.new(N, 0)
+fw = FenwickTree.new(N, Array.new(N,0))
 
-# dp[i]:=max(dp[j])+1
-# if boxes[i] > boxes[j]
-for row in 0...dp.length
-  for col in row...dp.length
-    if col == row
-      dp[row][col] = 1
-    else
-      bef = boxes[col-1]
-      cur = boxes[col]
-      puts "bef #{bef}"
-      puts "cur #{cur}"
-      if bef.keys.first < cur.keys.first and bef.values.first < cur.values.first
-        dp[row][col] = [dp[row].max, dp[row].max + 1].max
-      end
-    end
-    p dp[row]
-  end
+for i in 1...N
+  dp[i] = fw.max(1, i-1) + 1
+  fw.add(i, dp[i])
+  p dp
 end
 
-for idx in 0...dp.length
-  puts "[#{idx}] #{dp[idx]}"
-end
-
-p dp.map{|row| row.last}.max
+p dp.max
