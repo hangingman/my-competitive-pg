f916fb0ae206b9f6b8c5898887d912a265dc89d5
hiroyuki nagata <newserver002@gmail.com>
2018-06-27 23:44:07 +0900

    Porting digit DP code from C++

---
+++ algorithm/digit_db.rb
@@ -0,0 +1,33 @@
+lines = <<'EOS'
+4275631
+EOS
+
+#lines = $stdin.read
+array = lines.split("\n")
+A = array[0].split("").map(&:to_i)
+N = array[0].to_s.length
+
+MOD = 1_000_000_007
+dp = Array.new(101010).map{Array.new(2, 0)} # pos, less
+dp[0][0] = 1
+
+for i in 0...N
+  for j in 0...2
+    lim = j.zero? ? A[i] : 9
+    for d in 0...lim+1
+      less = if not j.zero? or d<lim
+               1
+             else
+               0
+             end
+      dp[i+1][less] += dp[i][j]
+    end
+  end
+end
+
+ans = 0
+for j in 0...2
+  ans += dp[N][j]
+end
+
+puts ans
