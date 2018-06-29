2f2c04036375f7bee9e55bb79b26c0a7e8cb4b67
Hiroyuki Nagata <hiroyuki.nagata@veeva.com>
2018-06-29 17:06:59 +0900

    Commit AtCoder ABC 013 - D ... RE(80/100)

--- atcoder/abc/013/D.rb
+++ atcoder/abc/013/D.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 lines = <<'EOS'
 5 7 2
 1 4 3 4 2 3 1
@@ -6,34 +7,48 @@ EOS
 #lines = $stdin.read
 array = lines.split("\n")
 
-def fact(n, a=1)
-  return a if n == 0
-  fact(n-1, n*a)
+def msb(x)
+  x.to_s(2).size - 1
 end
 
-N,M,D = array[0].split(" ").map(&:to_i)
-A     = array[1].split(" ").map(&:to_i)
+def lsb(x)
+  msb(x & -x)
+end
 
-st    = 1.upto(N).to_a
+N,M,Q = array[0].split(" ").map(&:to_i)
+A     = array[1].split(" ").map(&:to_i)
 to    = 1.upto(N).to_a
 
 for a in A.reverse
   to[a], to[a-1] = to[a-1],to[a]
 end
 
-if D != 1
-  d = D % fact(N)
-  1.upto(d) do |i|
-    #puts "concat! D =#{i}"
-    neo_to = Array.new(N)
+# doubling
+LOGN = msb(Q)+1
+next_obj = Array.new(LOGN+1).map{Array.new(N, 0)}
 
-    to.each_with_index do |elem,idx|
-      neo_to[idx] = st[elem-1]
+for i in 0...N
+  next_obj[0][i] = to[i] - 1
+end
+
+for k in 0...LOGN
+  for i in 0...N
+    if next_obj[k][i] == -1
+      next_obj[k+1][i] = -1
+    else
+      next_obj[k+1][i] = next_obj[k][next_obj[k][i]]
     end
-    st = neo_to
-    #puts st.to_s
   end
-  st.each{ |t| puts t }
-else
-  to.each{ |t| puts t }
+end
+
+QBIT = Q.to_s(2).reverse
+
+for p in 0...N
+  pidx = p
+  (1..LOGN).to_a.reverse_each do |k|
+    if 1 == QBIT[k-1].to_i
+      pidx = next_obj[k-1][pidx]
+    end
+  end
+  puts pidx+1
 end
