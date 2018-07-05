1e357384a7a96c3abb4ee9b653e975b4c28b5948
Hiroyuki Nagata <hiroyuki.nagata@veeva.com>
2018-07-05 13:54:03 +0900

    Commit AtCoder ABC 084 - D ...AC!

--- atcoder/abc/084/D.rb
+++ atcoder/abc/084/D.rb
@@ -1,31 +1,63 @@
-lines = <<'EOS'
-1
-3000 7000000
-EOS
+# coding: utf-8
 
-#lines = $stdin.read
+def erastosthenes_table(n)
+  # 0,1は素数(false)
+  checked = Array.new(n+1,false)
+  is_prime = Array.new(n+1,true)
 
-def erastosthenes(n)
-  arr = (2..n).to_a
-  threshold = Math.sqrt(n)
-  count = 2
-  while threshold > count
-    arr.reject!{|n| n != count and n % count == 0}
-    count += 1
+  is_prime[0] = false
+  is_prime[1] = false
+
+  for i in 2..(Math.sqrt(n).floor)
+    next if checked[i]
+
+    is_prime[i] = true
+    checked[i] = true
+
+    j = 2
+    for j in 2..(n/i)
+      checked[i*j] = true
+      is_prime[i*j] = false
+    end
   end
-  arr
+
+  is_prime
+end
+
+$pn_table = erastosthenes_table(10**5+1)
+$table = Array.new(10**5+1,0)
+
+def is_prime?(n)
+  $pn_table[n]
 end
 
 def like_2017?(n)
   is_prime?(n) and is_prime?((n+1)/2)
 end
 
+1.step(10**5,1).each do |e|
+  if like_2017?(e) and e.odd?
+    $table[e] = $table[e-1] + 1
+  else
+    $table[e] = $table[e-1]
+  end
+end
+
+#lines = <<'EOS'
+#6
+#1 53
+#13 91
+#37 55
+#19 51
+#73 91
+#13 49
+#EOS
+lines = $stdin.read
 array = lines.split("\n")
+
 Q = array[0].to_i
+
 array[1..Q].each do |str|
   l,r = str.split(" ").map(&:to_i)
-  arr = l.step(r, 2).to_a
-  pns = erastosthenes(r)
-  ans = arr & pns
-  puts ans.length
+  puts $table[r] - $table[l-1]
 end
