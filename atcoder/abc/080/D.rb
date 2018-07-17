09ade812c8a4c153c27a689095509a292e2c23e0
hiroyuki nagata <newserver002@gmail.com>
2018-07-18 00:25:19 +0900

    Commit AtCoder ABC 080 - D ... WA(11/22)

--- atcoder/abc/080/D.rb
+++ atcoder/abc/080/D.rb
@@ -1,30 +1,41 @@
+# coding: utf-8
 lines = <<'EOS'
-3 2
-1 7 2
-7 8 1
-8 12 1
+9 4
+56 60 4
+33 37 2
+89 90 3
+32 43 1
+67 68 3
+49 51 3
+31 32 3
+70 71 1
+11 12 3
 EOS
 
 #lines = $stdin.read
 array = lines.split("\n")
 
-TIME = 10**5
+TIME = 10**5*2
+#TIME = 30
 
 N,C = array[0].split(" ").map(&:to_i)
 
-vec = Array.new(TIME){0}
+cumsum = Array.new(C).map{Array.new(TIME,0)}
 
 array[1..N+1].each do |rec|
-  s,e,c  = rec.split(" ").map(&:to_i)
-  vec[s] = vec[s] + 1
-  vec[e+1] = vec[e+1] - 1 rescue vec[e+1]
+  s,t,c  = rec.split(" ").map(&:to_i)
+  s,t=s*2-1,t*2     # double
+  s,t,c=s-1,t-1,c-1 # 0-indexed
+  cumsum[c][s] += 1
+  cumsum[c][t] -= 1
 end
 
-ans = Array.new(vec.length){0}
-ans[0] = vec[0]
-
-for i in 1...ans.length
-  ans[i] = ans[i-1] + vec[i-1] rescue ans[i-1]
-end
+ans = cumsum.map! do |cumrow|
+  sum = cumrow.dup
+  for i in 1...cumrow.length
+    sum[i] += sum[i-1]
+  end
+  sum
+end.transpose.map{|row| row.inject(&:+)}
 
 puts ans.max
