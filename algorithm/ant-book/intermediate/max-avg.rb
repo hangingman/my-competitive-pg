b0be92f251b512415678859775ed2949ada15889
hiroyuki nagata <hiroyuki@cowboy.freestylewiki.xyz>
2018-08-08 14:09:05 +0900

    Add example ruby code of ant-book max-avarage with binary-search

---
+++ algorithm/ant-book/intermediate/max-avg.rb
@@ -0,0 +1,46 @@
+# n = 3
+# k = 2
+# (w,v) ={2,2},{5,3},{2,1}
+
+INF = Float::INFINITY
+
+class MaxAvg
+
+  attr :n,:k,:w,:v,:y
+  
+  def initialize(n,k,w,v)
+    @n=n
+    @k=k
+    @w=w.dup
+    @v=v.dup
+    @y=[]
+  end
+  
+  def C(x)
+    for i in 0..@n
+      @y[i]=@v[i]-x*@w[i]
+    end
+    @y=@y.sort
+    sum=0
+    for i in 0..@k
+      sum+=@y[n-i-1]
+    end
+    sum>=0
+  end
+
+  def solve()
+    lb,ub=0,INF
+    for i in 0..100
+      mid = (lb+ub)/2
+      if C(mid)
+        lb = mid 
+      else
+        ub = mid
+      end
+    end
+    puts "%.2f" % ub
+  end
+end
+
+ma = MaxAvg.new(3,2,[2,5,2],[2,3,1])
+ma.solve()
