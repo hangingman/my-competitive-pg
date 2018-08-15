b4a4098f813dc07fcfde832def8fd9fc756571db
Hiroyuki Nagata <hiroyuki.nagata@veeva.com>
2018-08-15 12:57:10 +0900

    Commit AtCoder ABC 105 D, AC

--- atcoder/abc/105/D.rb
+++ atcoder/abc/105/D.rb
@@ -1,6 +1,34 @@
+# coding: utf-8
 lines = <<'EOS'
-2 4
+3 2
+4 1 5
 EOS
 
 #lines = $stdin.read
 array = lines.split("\n")
+
+N,M = array[0].split(" ").map(&:to_i)
+A = array[1].split(" ").map(&:to_i)
+sums = 0
+nums = {}
+
+nums[0] = 1
+A.each.with_index do |e,i|
+  sums += e
+  # count mods
+  if nums[sums % M].nil?
+    nums[sums % M] = 1
+  else
+    nums[sums % M] += 1
+  end
+end
+
+#p A
+#p nums
+
+ans = 0
+nums.each do |k,v|
+  ans += v*(v-1)/2
+end
+
+puts ans
