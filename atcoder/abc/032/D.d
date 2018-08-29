f9c96aa28c7181e948c5839edbb132e42b4dea80
Hiroyuki Nagata <idiotpanzer@gmail.com>
2018-08-29 16:05:51 +0900

    ABC 032 - D by dlang

--- atcoder/abc/032/D.d
+++ atcoder/abc/032/D.d
@@ -6,8 +6,9 @@ import std.string;
 import std.format;
 import std.algorithm;
 import std.typecons;
+import std.array;
 
-alias Tuple!(int, int) pair;
+alias Tuple!(ulong, ulong) pair;
 
 void stringsTo(string, T...)(string str, ref T t) {
   auto s = str.split();
@@ -20,7 +21,44 @@ void stringsTo(string, T...)(string str, ref T t) {
 
 void meet_in_the_mid(int n, int w, pair[] vw)
 {
+  int s = 0;
+  pair[] s1 = vw[0..n/2-1];
+  pair[] s2 = vw[n/2..n];
+  pair[] tmp_a;
 
+  for (int bit = 0; bit < (1<<s1.length); bit++) {
+    pair tmp_pair = pair(0,0);
+    foreach (i; 0..s1.length) {
+      if (bit & (1<<i)) {
+	tmp_pair[0] += s1[i][0];
+	tmp_pair[1] += s1[i][1];
+      }
+    }
+    if (tmp_pair[1] <= w) {
+      tmp_a ~= tmp_pair;
+    }
+  }
+
+  sort!( (a,b) => a[0] > b[0] )(tmp_a);
+
+  ulong ans = 0;
+  for (int bit = 0; bit < (1<<s2.length); bit++) {
+    pair tmp_pair = pair(0,0);
+    foreach (i; 0..s2.length) {
+      if (bit & (1<<i)) {
+	tmp_pair[0] += s2[i][0];
+	tmp_pair[1] += s2[i][1];
+      }
+    }
+
+    if (tmp_pair[1] <= w) {
+      pair[] x = tmp_a.find!( (e) => tmp_pair[1] + e[1] <= w );
+      if (! x.empty) {
+	ans = reduce!(max)([ans, x[0][0] + tmp_pair[0]]);
+      }
+    }
+  }
+  writefln("%d", ans);
 }
 
 void solve_sigma_v(int n, int w, pair[] vw, int max_v)
@@ -60,15 +98,15 @@ void main()
     vw[i-1] = p;
   }
 
-  if (N > 30) {
+  if (N <= 30) {
     // meet in the mid
     meet_in_the_mid(N, W, vw);
   } else {
-    // 01 knapsack
-    if (max_v <= 1000) {
-      solve_sigma_v(N, W, vw, max_v);
-    } else {
-      solve_sigma_w(N, W, vw, max_w);
-    }
+    //  // 01 knapsack
+    //  if (max_v <= 1000) {
+    //    solve_sigma_v(N, W, vw, max_v);
+    //  } else {
+    //    solve_sigma_w(N, W, vw, max_w);
+    //  }
   }
 }
