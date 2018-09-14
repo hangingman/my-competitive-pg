861ad579191cd19ffe75a562ac0595acdfc4e5dd
Hiroyuki Nagata <hiroyuki.nagata@veeva.com>
2018-09-14 13:21:03 +0900

    Commit AtCoder ABC 011 ... D

--- atcoder/abc/011/D.rb
+++ atcoder/abc/011/D.rb
@@ -9,7 +9,6 @@ class Combination
   end
 
   def choose(n, r)
-    puts "#{n}C#{r}"
     return 0 if not (0 <= r and r <= n)
     return 1 if r == 0 or r == n
     fact(n) * inverse( fact(r) * fact(n-r) % MOD ) % MOD
@@ -35,8 +34,8 @@ class Combination
 end
 
 lines = <<'EOS'
-11 8562174
-25686522 17124348
+2 10000000
+10000000 10000000
 EOS
 
 #lines = $stdin.read
@@ -47,20 +46,19 @@ X,Y = array[1].split(" ").map(&:to_i)
 ans  = 0
 comb = Combination.new(N*D)
 
-for k in 1..N
-  # p k
+for k in 0..N
   lr_move_k = k*D
   ud_move_k = (N-k)*D
-  #p ud_move_k
 
   lr_r = (lr_move_k+X)/2
   ud_r = (ud_move_k+Y)/2
-  #p ud_r
 
+  nu = comb.choose(N, N-k)
   lr = comb.choose(lr_move_k, lr_r)
   ud = comb.choose(ud_move_k, ud_r)
-  # puts "#{lr_move_k}C#{lr_r}=#{left} + #{ud_move_k}C#{ud_r}=#{right}"
-  ans += (lr*ud)
+
+  puts "#{N}C#{N-k}=#{nu} * #{lr_move_k}C#{lr_r}=#{lr} * #{ud_move_k}C#{ud_r}=#{ud}"
+  ans += (nu*lr*ud)
 end
 
 puts ans.quo(4**N).to_f
