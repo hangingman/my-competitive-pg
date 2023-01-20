9c7a196cc408ac3204ec31cb328a890673d520c5
hiroyuki.nagata <idiotpanzer@gmail.com>
2023-01-20 21:32:31 +0900

    Update atcoder/abc/011/D.rb

--- atcoder/abc/011/D.rb
+++ atcoder/abc/011/D.rb
@@ -1,71 +1,71 @@
-MOD = 1_000_000_007
-
-class Combination
-  def initialize(n)
-    @fact = (1..n).to_a
-    @fact.each_with_index do |e, idx|
-      @fact[idx] = @fact[idx] * @fact[idx-1] % MOD if idx > 0
-    end
-  end
-
-  def choose(n, r)
-    return 0 if not (0 <= r and r <= n)
-    return 1 if r == 0 or r == n
-    fact(n) * inverse( fact(r) * fact(n-r) % MOD ) % MOD
-  end
-
-  def fact(n)
-    @fact[n-1]
-  end
-
-  def inverse(x)
-    pow(x, MOD-2)
-  end
-
-  def pow(x, n)
-    ans = 1
-    while n > 0
-      ans = ans * x % MOD if n.odd?
-      x = x * x % MOD
-      n >>= 1
-    end
-    ans
-  end
-end
-
 lines = <<'EOS'
-2 10000000
-10000000 10000000
+11 8562174
+25686522 17124348
 EOS
 
 #lines = $stdin.read
 array = lines.split("\n")
+
 N,D = array[0].split(" ").map(&:to_i)
-X,Y = array[1].split(" ").map(&:to_i)
+x,y = array[1].split(" ").map(&:to_i)
 
-comb = Combination.new(D)
-
-ans  = 0
-shortest = comb.choose(N, X/D)
-
-if (X%D) != 0 or (Y%D) != 0
+if (x%D != 0 || y%D !=0)
   puts 0.0
-else
-  for k in 0..N
-    lr_move_k = k*D
-    ud_move_k = (N-k)*D
-
-    next if lr_move_k < X or ud_move_k < Y
-
-    lr_r = (lr_move_k+X)/2
-    ud_r = (ud_move_k+Y)/2
-
-    lr = comb.choose(lr_move_k, lr_r)
-    ud = comb.choose(ud_move_k, ud_r)
-
-    tmp = (shortest*lr*ud)
-    ans += tmp
-    puts "#{shortest} * #{lr_move_k}C#{lr_r}=#{lr} * #{ud_move_k}C#{ud_r}=#{ud} = #{tmp}"
-  end
-  puts ans.quo(4**N).to_f
+  exit 0
 end
+
+X,Y = array[1].split(" ").map(&:to_i).map{|n| n/D}
+MAX_N=1000
+
+class Combination
+  attr_reader :comb
+
+  def initialize
+    @comb = Array.new(MAX_N+1).map{Array.new(MAX_N+1, 0.to_f)}
+    @comb[0][0] = 1.to_f
+
+    for i in 0...MAX_N
+      for j in 0...MAX_N
+        #puts "i=#{i},j=#{j}"
+        #puts "---"
+        @comb[i+1][j+1] += @comb[i][j].quo(2).to_f
+        @comb[i+1][j] += @comb[i][j].quo(2).to_f
+      end
+    end
+  end
+
+  def prob(n,r)
+    @comb[n][r]
+  end
+end
+
+@c = Combination.new
+ans = 0
+
+for a in 0..N
+  b = N - a
+  # puts "x move: #{a}, y move: #{b}"
+  # puts "(a+X)%2 = #{(a+X)%2}"
+  # puts "(b+Y)%2 = #{(b+Y)%2}"
+  next if (a+X)%2 == 1 || (b+Y)%2 == 1
+
+  #puts "a=#{a},b=#{b},X=#{X},Y=#{Y}"
+  #puts "---"
+  r=(a + X).quo(2)
+  l=(a - r)
+  u=(b + Y).quo(2)
+  d=(b - u)
+
+  next if r<0 or l<0 or u<0 or d<0
+  # p "r:#{r},l:#{l}"
+  # p "u:#{u},d:#{d}"
+  # p "@c.prob(#{N},#{a}) = #{@c.prob(N,a)}"
+  # p "@c.prob(#{a},#{r}) = #{@c.prob(a,r)}"
+  # p "@c.prob(#{b},#{u}) = #{@c.prob(b,u)}"
+
+  tmp = @c.prob(N,a) * @c.prob(a,r.to_i) * @c.prob(b,u.to_i)
+  # p "tmp = #{tmp}"
+  ans += tmp
+end
+
+printf("%.10f\n", ans.to_f)
