14f67a88e791a8bd7e0f9baaab4f2d7446b52586
hiroyuki.nagata <idiotpanzer@gmail.com>
2024-06-09 09:29:02 +0900

    メモリー部分の実装 aider: Implemented save, load, and clear methods using ConversationTokenBufferMemory with a default buffer size of 2000.

--- CompeteAI/src/CompeteAI/infra/memory/memory.py
+++ CompeteAI/src/CompeteAI/infra/memory/memory.py
@@ -1,6 +1,16 @@
-from langchain.memory import Memory
+from langchain.memory import ConversationTokenBufferMemory
 
 
-class CustomMemory(Memory):
+class CustomMemory:
     def __init__(self):
         super().__init__()
+        self.memory = ConversationTokenBufferMemory(buffer_size=2000)
+
+    def save_context(self, inputs, outputs):
+        self.memory.save_context(inputs, outputs)
+
+    def load_context(self):
+        return self.memory.load_context()
+
+    def clear(self):
+        self.memory.clear()
